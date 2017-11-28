22f5adb2e88a93bd62a860da991ed0596c35df2a
jkrmnj <jkrmnj465@gmail.com>
2017-11-28 09:09:14 -0700

    WorkingIsh shadow

--- geometry.py
+++ geometry.py
@@ -12,37 +12,44 @@ class Vector:
         self.x = x
         self.y = y
         self.z = z
+
     def __add__(self, other):
         return Vector(self.x+other.x, self.y+other.y, self.z+other.z)
+
     def __mul__(self, other):
         if (isinstance(other, Vector)):
             return self.x * other.x + self.y * other.y + self.z * other.z
         else:
             return Vector(self.x * other, self.y * other, self.z * other)
+
     def __sub__(self, other):
         return self + other * -1
+
+    #Cross product
     def __pow__(self, other):
         return Vector(self.y * other.z - self.z * other.y, self.z * other.x - self.x * other.z, self.x * other.y - self.y * other.x)
+
     def __str__(self):
         return "" + str(self.x) + " " + str(self.y) + " " + str(self.z) + ""
-
 
 class Ray:
     location: Vector
     direction: Vector
+
     def __init__(self, loc, dir):
         self.location = loc
         self.direction = dir
+
     def __str__(self):
         return str(self.location) + " " + str(self.direction)
 
+#Basically classes without functions
 Plane = namedtuple("Plane", "location direction")
-Triangle = namedtuple("Triangle", "a b c, color")
-
-a = Plane(Vector(3, 1, 1), Vector(1, 1, 1))
+Triangle = namedtuple("Triangle", "a b c color")
 
 def intersection(plane, ray):
     return (plane.location - ray.location) * plane.direction / (ray.direction * plane.direction)
+
 def colliding(plane, ray):
     try:
         t = intersection(plane, ray)
@@ -52,8 +59,10 @@ def colliding(plane, ray):
             return True
     except:
         return False
+
 def rayToVector(r):
     return r.direction + r.location
+
 def isBounded(p, bounds, n, t):
     p = rayToVector(p) * t
     if ((bounds.b - bounds.a) ** (p - bounds.a)) * n > 0:
@@ -65,34 +74,36 @@ def isBounded(p, bounds, n, t):
             if ((bounds.a - bounds.c) ** (p - bounds.c)) * n < 0:
                 return True
     return False
+
 def triToPlane(triangle):
     normal = (triangle.a - triangle.b) ** (triangle.b - triangle.c)
     return normal * (1/sqrt(normal.x ** 2 + normal.y ** 2 + normal.z ** 2))
+
 def rayCast(ray, triangles, primary = True, self = None):
     shortest = 100000
     color = Vector(255, 255, 255)
+    tri = None
     for triangle in triangles:
         if triangle == self:
-            continue
+            pass
         p = Plane(triangle.a, triToPlane(triangle))
         if colliding(p, ray):
             time = intersection(p, ray)
             if isBounded(ray, triangle, p.direction, time):
                 if intersection(p, ray) < shortest:
                     shortest = time
+                    tri = triangle
                     if primary:
-                        secondT = rayCast(Ray(Vector(100, -100, 0), (rayToVector(ray) * time + p.direction * 0) - Vector(100, -100, 0)), triangles, False, triangle)
-                        if secondT[1] != 100000:
-                            if str(secondT[0]) == "255 0 0" and str(triangle.color) == "0 255 255":
-                                #print(str(Ray(rayToVector(ray) * time + p.direction * 1e-3, Vector(-1, 1, 0))))
-                                pass
-                            color = secondT[0] #triangle.color - Vector(100, 100, 100)
+                        secondT = rayCast(Ray(Vector(-1000, 100, 100), (rayToVector(ray) * time + p.direction * 0) - Vector(-1000, 100, 100)), triangles, False, triangle)
+                        if secondT[1] != 100000 and triangle != secondT[2]:
+                            color = triangle.color - Vector(100, 100, 100)
                         else:
                             color = triangle.color 
                     else:
                         color = triangle.color 
-    color = Vector(max(color.x, 0), max(color.y, 0), max(color.z, 0))
-    return (color, shortest)
+    if color.y < 0 or color.z < 0 or color.x < 0:
+        color = Vector(max(color.x, 0), max(color.y, 0), max(color.z, 0))
+    return (color, shortest, tri)
 
 def render(height, width, fov, tris):
     result = []
@@ -114,7 +125,7 @@ p = Vector(0.6, 2, 0.4)
 scene = list(map(lambda a: str(a), render(200, 200, pi / 3.5, [t, tt, ttt, floor])))
 print("P3")
 print("200 200")
-print("1")
+print("255")
 def chunker(seq, size):
     return (seq[pos:pos + size] for pos in range(0, len(seq), size))
 for line in chunker(scene, 200):
